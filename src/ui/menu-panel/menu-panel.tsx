0cbac0101a3ac6b584d1d381574bf79c1b024383
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-04-26 17:46:58 +0300

    buttons pane upd ref

--- src/ui/menu-panel/menu-panel.tsx
+++ src/ui/menu-panel/menu-panel.tsx
@@ -26,31 +26,45 @@ export const MenuPanel: React.FC<OwnProps> = () => {
 
     // легче редактировать и меньше кода на перебор
     const menuItems = [
-        {route: routes.login, src: loginSVG, title: 'Авторизация',
-            buttonText: `${isAuth ? 'Вход' : 'Выход'}`, active: true},
-        {route: routes.create, src: createSVG, title: 'Создать заявку',
-            buttonText: 'Создать', active: isAuth},
-        {route: routes.search, src: searchSVG, title: 'Поиск неактивных заявок',
-            buttonText: 'Поиск',active: isAuth},
-        {route: routes.status, src: statusSVG, title: 'Активные заявки',
-            buttonText: 'Заявки',active: isAuth},
-        {route: routes.history, src: historySVG, title: 'Выполненные заявки',
-            buttonText: 'История заявок',active: isAuth},
-        {route: routes.map, src: mapSVG, title: 'Карта маршрутов',
-            buttonText: 'Карта',active: isAuth},
-        {route: routes.options, src: optionsSVG, title: 'Панель настроек (админ)',
-            buttonText: 'Настройки',active: isAuth},
+        {
+            route: routes.login, src: loginSVG, title: 'Авторизация',
+            buttonText: `${isAuth ? 'Вход' : 'Выход'}`, active: true
+        },
+        {
+            route: routes.create, src: createSVG, title: 'Создать заявку',
+            buttonText: 'Создать', active: isAuth
+        },
+        {
+            route: routes.search, src: searchSVG, title: 'Поиск неактивных заявок',
+            buttonText: 'Поиск', active: isAuth
+        },
+        {
+            route: routes.status, src: statusSVG, title: 'Активные заявки',
+            buttonText: 'Заявки', active: isAuth
+        },
+        {
+            route: routes.history, src: historySVG, title: 'Выполненные заявки',
+            buttonText: 'История заявок', active: isAuth
+        },
+        {
+            route: routes.map, src: mapSVG, title: 'Карта маршрутов',
+            buttonText: 'Карта', active: isAuth
+        },
+        {
+            route: routes.options, src: optionsSVG, title: 'Панель настроек (админ)',
+            buttonText: 'Настройки', active: isAuth
+        },
     ]
 
     return (
         <nav className={styles.menuPanel}>
             {menuItems.map(({route, src, title, buttonText, active}) =>
-                active&&<div className={styles.menuPanel__item}>
-                    <NavLink to={route} className={activeClass} role={'button'} title={title}>
-                        <img className={styles.menuPanel__image} src={src} alt={buttonText}/>
-                        <div className={styles.menuPanel__text}>{buttonText}</div>
-                    </NavLink>
-                </div>
+                    active && <div className={styles.menuPanel__item}>
+                        <NavLink to={route} className={activeClass} role={'button'} title={title}>
+                            <img className={styles.menuPanel__image} src={src} alt={buttonText}/>
+                            <div className={styles.menuPanel__text}>{buttonText}</div>
+                        </NavLink>
+                    </div>
             )}
         </nav>
     )
