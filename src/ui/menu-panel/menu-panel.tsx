c4b72e30677ae20ab5c4638c11c3fa57122b6a16
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-04-26 17:44:46 +0300

    buttons pane upd active

--- src/ui/menu-panel/menu-panel.tsx
+++ src/ui/menu-panel/menu-panel.tsx
@@ -26,19 +26,26 @@ export const MenuPanel: React.FC<OwnProps> = () => {
 
     // легче редактировать и меньше кода на перебор
     const menuItems = [
-        {route: routes.login, src: loginSVG, title: 'Авторизация', buttonText: `${isAuth ? 'Вход' : 'Выход'}`},
-        {route: routes.create, src: createSVG, title: 'Создать заявку', buttonText: 'Создать'},
-        {route: routes.search, src: searchSVG, title: 'Поиск неактивных заявок', buttonText: 'Поиск'},
-        {route: routes.status, src: statusSVG, title: 'Активные заявки', buttonText: 'Заявки'},
-        {route: routes.history, src: historySVG, title: 'Выполненные заявки', buttonText: 'История заявок'},
-        {route: routes.map, src: mapSVG, title: 'Карта маршрутов', buttonText: 'Карта'},
-        {route: routes.options, src: optionsSVG, title: 'Панель настроек (админ)', buttonText: 'Настройки'},
+        {route: routes.login, src: loginSVG, title: 'Авторизация',
+            buttonText: `${isAuth ? 'Вход' : 'Выход'}`, active: true},
+        {route: routes.create, src: createSVG, title: 'Создать заявку',
+            buttonText: 'Создать', active: isAuth},
+        {route: routes.search, src: searchSVG, title: 'Поиск неактивных заявок',
+            buttonText: 'Поиск',active: isAuth},
+        {route: routes.status, src: statusSVG, title: 'Активные заявки',
+            buttonText: 'Заявки',active: isAuth},
+        {route: routes.history, src: historySVG, title: 'Выполненные заявки',
+            buttonText: 'История заявок',active: isAuth},
+        {route: routes.map, src: mapSVG, title: 'Карта маршрутов',
+            buttonText: 'Карта',active: isAuth},
+        {route: routes.options, src: optionsSVG, title: 'Панель настроек (админ)',
+            buttonText: 'Настройки',active: isAuth},
     ]
 
     return (
         <nav className={styles.menuPanel}>
-            {menuItems.map(({route, src, title, buttonText}) =>
-                <div className={styles.menuPanel__item}>
+            {menuItems.map(({route, src, title, buttonText, active}) =>
+                active&&<div className={styles.menuPanel__item}>
                     <NavLink to={route} className={activeClass} role={'button'} title={title}>
                         <img className={styles.menuPanel__image} src={src} alt={buttonText}/>
                         <div className={styles.menuPanel__text}>{buttonText}</div>
