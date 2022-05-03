613afddc8344143f943f110faac505a5de24432d
Otto-Vector <otto-vector@yandex.ru>
2022-05-03 21:02:59 +0300

    hello-section ref

--- src/ui/hello-section/description-insider/description-insider.tsx
+++ src/ui/hello-section/description-insider/description-insider.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import styles from './description-insider.module.scss';
+import React from 'react'
+import styles from './description-insider.module.scss'
 
 type OwnProps = {
     srcIcon: string
     textArray: string[]
-    position: 'left' | 'right' | 'center'
+    position?: 'left' | 'right' | 'center'
+    hide?: boolean
 }
 
-export const DescriptionInsider: React.FC<OwnProps> = ({srcIcon, textArray, position}) => {
+export const DescriptionInsider: React.FC<OwnProps> = ( { srcIcon, textArray, position = 'center', hide = true } ) => {
 
-    const positionIs = (pos: OwnProps['position']): string =>
-      pos === 'left' ? styles.descriptionInsider_left
-          : pos === 'right' ? styles.descriptionInsider_right
-              : ''
+    const positionIs = ( pos: OwnProps['position'] ): string =>
+        pos === 'left' ? styles.descriptionInsider_left
+            : pos === 'right' ? styles.descriptionInsider_right
+                : ''
+
+    const viewedIs = ( trigger: boolean ): string => trigger ? '' : styles.descriptionInsider_viewed
 
     return (
-        <div className={styles.descriptionInsider + ' ' + positionIs(position)}>
-            <img className={styles.descriptionInsider__icon} src={srcIcon} alt="hidden"/>
-            {textArray.map((text) =>
-                <p className={styles.descriptionInsider__text} key={text}>{text}</p>)}
+        <div className={ styles.descriptionInsider + ' ' + positionIs( position ) + ' ' + viewedIs( hide ) }>
+            <img className={ styles.descriptionInsider__icon } src={ srcIcon } alt="hidden"/>
+            { textArray.map( ( text ) =>
+                <p className={ styles.descriptionInsider__text } key={ text }>{ text }</p> ) }
         </div>
     )
 }
--- src/ui/hello-section/hello-section.tsx
+++ src/ui/hello-section/hello-section.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import styles from './hello-section.module.scss';
+import React, {useState} from 'react'
+import styles from './hello-section.module.scss'
 
 // иконки
 import man from './helloSectionSVG/man.svg'
@@ -7,12 +7,26 @@ import portfeler from './helloSectionSVG/briefcase.svg'
 import track from './helloSectionSVG/track.svg'
 import car from './helloSectionSVG/car.svg'
 import factory from './helloSectionSVG/factory.svg'
-import {DescriptionInsider} from './description-insider/description-insider';
+import {DescriptionInsider} from './description-insider/description-insider'
 
 
 type OwnProps = {}
 
 export const HelloSection: React.FC<OwnProps> = () => {
+
+    const initial = {
+        man: true,
+        portfeler: true,
+        track: true,
+        car: true,
+        factory: true,
+    }
+
+    const [ view, setView ] = useState( initial )
+
+    const activeView = ( key: keyof typeof initial ) => {
+        setView( { ...initial, [key]: false } )
+    }
 
     const description = {
         man: [
@@ -65,35 +79,47 @@ export const HelloSection: React.FC<OwnProps> = () => {
     // ToDo: helloItem в отдельный блок
 
     return (
-        <section className={styles.helloSection}>
-            <h1 className={styles.helloSection__title}>Новые возможности логистики с ТЛК</h1>
-            <p className={styles.helloSection__subtitle}>Сервис транспортировки нефтепродуктов и прочих грузов</p>
-            <p className={styles.helloSection__subtitle}>Полная автоматизация грузоперевозок на одном сайте</p>
-            <div className={styles.helloSection__bottomWrapper}>
-                <div className={styles.helloItem}>
-                    <DescriptionInsider srcIcon={man} textArray={description.man} position={'left'}/>
-                    <img className={styles.helloItem__icon} src={man} alt="Выгода"/>
-                    <span className={styles.helloItem__title}>{'Выгода Заказчикам'}</span>
+        <section className={ styles.helloSection } onClick={ () => {
+            setView( initial )
+        } }>
+            <h1 className={ styles.helloSection__title }>Новые возможности логистики с ТЛК</h1>
+            <p className={ styles.helloSection__subtitle }>Сервис транспортировки нефтепродуктов и прочих грузов</p>
+            <p className={ styles.helloSection__subtitle }>Полная автоматизация грузоперевозок на одном сайте</p>
+            <div className={ styles.helloSection__bottomWrapper }>
+                <DescriptionInsider srcIcon={ man } textArray={ description.man } hide={ view.man }/>
+                <DescriptionInsider srcIcon={ portfeler } textArray={ description.portfeler } hide={ view.portfeler }/>
+                <DescriptionInsider srcIcon={ track } textArray={ description.track } hide={ view.track }/>
+                <DescriptionInsider srcIcon={ car } textArray={ description.car } hide={ view.car }/>
+                <DescriptionInsider srcIcon={ factory } textArray={ description.factory } hide={ view.factory }/>
+                <div className={ styles.helloItem } onMouseEnter={ () => {
+                    activeView( 'man' )
+                } }>
+                    <img className={ styles.helloItem__icon } src={ man } alt="Выгода"/>
+                    <span className={ styles.helloItem__title }>{ 'Выгода Заказчикам' }</span>
                 </div>
-                <div className={styles.helloItem}>
-                    <DescriptionInsider srcIcon={portfeler} textArray={description.portfeler} position={'left'}/>
-                    <img className={styles.helloItem__icon} src={portfeler} alt="Преимущество"/>
-                    <span className={styles.helloItem__title}>{'Преимущество Поставщикам'}</span>
+                <div className={ styles.helloItem } onMouseEnter={ () => {
+                    activeView( 'portfeler' )
+                } }>
+                    <img className={ styles.helloItem__icon } src={ portfeler } alt="Преимущество"/>
+                    <span className={ styles.helloItem__title }>{ 'Преимущество Поставщикам' }</span>
                 </div>
-                <div className={styles.helloItem}>
-                    <DescriptionInsider srcIcon={track} textArray={description.track} position={'center'}/>
-                    <img className={styles.helloItem__icon} src={track} alt="Возможности"/>
-                    <span className={styles.helloItem__title}>{'Возможности Перевозчикам'}</span>
+                <div className={ styles.helloItem } onMouseEnter={ () => {
+                    activeView( 'track' )
+                } }>
+                    <img className={ styles.helloItem__icon } src={ track } alt="Возможности"/>
+                    <span className={ styles.helloItem__title }>{ 'Возможности Перевозчикам' }</span>
                 </div>
-                <div className={styles.helloItem}>
-                    <DescriptionInsider srcIcon={car} textArray={description.car} position={'right'}/>
-                    <img className={styles.helloItem__icon} src={car} alt="Комфорт"/>
-                    <span className={styles.helloItem__title}>{'Комфорт Водителям'}</span>
+                <div className={ styles.helloItem } onMouseEnter={ () => {
+                    activeView( 'car' )
+                } }>
+                    <img className={ styles.helloItem__icon } src={ car } alt="Комфорт"/>
+                    <span className={ styles.helloItem__title }>{ 'Комфорт Водителям' }</span>
                 </div>
-                <div className={styles.helloItem}>
-                    <DescriptionInsider srcIcon={factory} textArray={description.factory} position={'right'}/>
-                    <img className={styles.helloItem__icon} src={factory} alt="Удобство"/>
-                    <span className={styles.helloItem__title}>{'Удобство Контрагентам'}</span>
+                <div className={ styles.helloItem } onMouseEnter={ () => {
+                    activeView( 'factory' )
+                } }>
+                    <img className={ styles.helloItem__icon } src={ factory } alt="Удобство"/>
+                    <span className={ styles.helloItem__title }>{ 'Удобство Контрагентам' }</span>
                 </div>
             </div>
         </section>
