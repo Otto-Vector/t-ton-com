f4e1f33834a2cd2371ba222461581c872a9f05c3
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-06-03 00:49:14 +0300

    info panel styled fix

--- src/ui/menu-panel/menu-panel.tsx
+++ src/ui/menu-panel/menu-panel.tsx
@@ -18,7 +18,6 @@ import {getIsAuthAuthStore} from '../../selectors/auth-reselect';
 import {getUnreadMessagesCountInfoStore} from '../../selectors/info-reselect';
 
 
-
 type OwnProps = {}
 
 export const MenuPanel: React.FC<OwnProps> = () => {
@@ -27,57 +26,57 @@ export const MenuPanel: React.FC<OwnProps> = () => {
     const isAuth = useSelector(getIsAuthAuthStore)
     const unreadMessagesCount = useSelector(getUnreadMessagesCountInfoStore)
     // вынес за пределы NavLink назначение классов
-    const activeClass = ({isActive}: { isActive: boolean }): string =>
-        `${styles.menuPanel__item} ${isActive
-            ? styles.menuPanel__item_active : styles.menuPanel__item_unactive}`
+    const activeClass = ( { isActive }: { isActive: boolean } ): string =>
+        `${ styles.menuPanel__item } ${ isActive
+            ? styles.menuPanel__item_active : styles.menuPanel__item_unactive }`
 
     // легче редактировать и меньше кода на перебор
     const menuItems = [
         {
             route: routes.login, src: loginSVG, title: 'Авторизация',
-            buttonText: `${isAuth ? 'Вход' : 'Выход'}`, active: true
+            buttonText: `${ isAuth ? 'Вход' : 'Выход' }`, active: true,
         },
         {
             route: routes.requestInfo.create, src: createSVG, title: 'Создать заявку',
-            buttonText: 'Создать', active: isAuth
+            buttonText: 'Создать', active: isAuth,
         },
         {
             route: routes.searchList, src: searchSVG, title: 'Поиск неактивных заявок',
-            buttonText: 'Поиск', active: isAuth
+            buttonText: 'Поиск', active: isAuth,
         },
         {
             route: routes.requestsList, src: statusSVG, title: 'Активные заявки',
-            buttonText: 'Заявки', active: isAuth
+            buttonText: 'Заявки', active: isAuth,
         },
         {
             route: routes.historyList, src: historySVG, title: 'Выполненные заявки',
-            buttonText: 'История заявок', active: isAuth
+            buttonText: 'История заявок', active: isAuth,
         },
         {
             route: routes.map, src: mapSVG, title: 'Карта маршрутов',
-            buttonText: 'Карта', active: isAuth
+            buttonText: 'Карта', active: isAuth,
         },
         {
             route: routes.options, src: optionsPNG, title: 'Панель настроек (админ)',
-            buttonText: 'Настройки', active: isAuth
+            buttonText: 'Настройки', active: isAuth,
         },
         {
             route: routes.info, src: infoSVG, title: 'Информация / События',
-            buttonText: 'Инфо', active: isAuth
+            buttonText: 'Инфо', active: isAuth,
         },
     ]
 
     return (
-        <nav className={styles.menuPanel}>
-            {menuItems.map(({route, src, title, buttonText, active}) =>
+        <nav className={ styles.menuPanel }>
+            { menuItems.map(( { route, src, title, buttonText, active } ) =>
                 active &&
-                <NavLink to={route} className={activeClass} role={'button'} title={title} key={route+src}>
-                    <img className={styles.menuPanel__image} src={src} alt={buttonText}/>
-                    <div className={styles.menuPanel__text}>{buttonText}</div>
-                    {(buttonText === 'Инфо' && unreadMessagesCount !== 0) &&
-                        <div className={styles.attentionIcon}><img src={attentionSVG} alt={'!'}/></div>}
-                </NavLink>
-            )}
+                <NavLink to={ route } className={ activeClass } role={ 'button' } title={ title } key={ route + src }>
+                    <img className={ styles.menuPanel__image } src={ src } alt={ buttonText }/>
+                    <div className={ styles.menuPanel__text }>{ buttonText }</div>
+                    { ( buttonText === 'Инфо' && unreadMessagesCount !== 0 ) &&
+                        <div className={ styles.attentionIcon }><img src={ attentionSVG } alt={ '!' }/></div> }
+                </NavLink>,
+            ) }
         </nav>
     )
 }
