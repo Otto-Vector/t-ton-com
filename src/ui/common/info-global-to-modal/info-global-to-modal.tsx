5e9e1d0bf7f742d4e8548834605f0f40c89ff7bc
Otto-Vector <50682440+Otto-Vector@users.noreply.github.com>
2022-10-12 23:04:15 +0300

    add action to modal 2

--- src/ui/common/info-global-to-modal/info-global-to-modal.tsx
+++ src/ui/common/info-global-to-modal/info-global-to-modal.tsx
@@ -1,15 +1,12 @@
 import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux';
-import {globalModalStoreActions, globalModalStoreReducer} from '../../../redux/utils/global-modal-store-reducer';
+import {globalModalStoreActions} from '../../../redux/utils/global-modal-store-reducer';
 // import 'antd/lib/style/index.css' // используем core стили antd
 // import 'antd/lib/modal/style/index.css' // используем стили antd для модальных инфо-окон
 // import 'antd/lib/button/style/index.css' // используем стили antd для кнопок
 import {Modal} from 'antd';
-import {useNavigate} from 'react-router-dom';
 import {
     getActionGlobalModalStore,
-    getIsOkHandleGlobalModalStore,
-    getNavigateToOkGlobalModalStore,
     getTextGlobalModalStore,
 } from '../../../selectors/utils/global-modal-reselect';
 
@@ -17,25 +14,25 @@ import {
 export const InfoGlobalToModal: React.FC = () => {
 
     const textToGlobalModal = useSelector(getTextGlobalModalStore)
-    const navToOk = useSelector(getNavigateToOkGlobalModalStore)
     const action = useSelector(getActionGlobalModalStore)
-
     const dispatch = useDispatch()
-    const navigate = useNavigate()
+
+    // const navToOk = useSelector(getNavigateToOkGlobalModalStore)
+    // const navigate = useNavigate()
 
     const [ visible, setVisible ] = useState(false)
 
     const onCloseLocal = () => {
         setVisible(false)
         dispatch(globalModalStoreActions.setTextMessage(''))
+        dispatch(globalModalStoreActions.setAction(null))
+        dispatch(globalModalStoreActions.setNavigateToOk(''))
     }
 
     const onOkHandle = () => {
         onCloseLocal()
         // dispatch(globalModalStoreActions.setIsOkHandle(true))
-        // dispatch(globalModalStoreActions.setNavigateToOk(''))
         // navToOk && navigate(navToOk)
-        dispatch(globalModalStoreActions.setAction(null))
         action && dispatch<any>(action())
     }
 
